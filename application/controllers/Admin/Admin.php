a36c579f47e6d8f060f0c9f9dc867c3a63e8516a
raduparvan <parvanraducu@gmail.com>
2015-04-17 11:37:51 +0300

    am sters testele

--- application/controllers/Admin/Admin.php
+++ application/controllers/Admin/Admin.php
@@ -3,23 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	public function test(){
-		$this->load->view('test');
-	}
-	public function test2(){
-
-		$this->load->view('welcome_message');
-
-		$this->load->view('test');
-		//test
-
-	}
-
-	public function test3(){
-	    $this->load->view('test');
-	}
-	public function test4(){
-		$this->load->view('test');
-
-	}
+    public function index(){
+    	
+    }
 }
